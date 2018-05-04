f9ce0f3fe79c98101864d6224f37e2f7f4a46fd0
Guru Manoj <amgurumanoj@gmail.com>
2018-05-04 17:17:38 +0530

    expect j

--- character_segmentation.py
+++ character_segmentation.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("/home/guru/Desktop/work/projects/chacter_segmentation/5.jpg",0)
+img = cv2.imread("/home/guru/Desktop/work/projects/chacter_segmentation/9.JPG",0)
 ret,res = cv2.threshold(img,5,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 a= res.shape
 print(a)
 x=[]
+x.append(0)
 for i in range(a[1]):
   for j in range(a[0]):
   	if res[j][i] == 255:
@@ -16,12 +17,14 @@ print(x)
 res = np.array(res)
 im=[]
 for i in range(len(x)-1):
-  b = res[0:29, x[i]:x[i+1]]
+  b = res[0:a[0]-1, x[i]:x[i+1]]
   if np.any(b==255):
     im.append(b)
 cv2.imshow("img",img)
 cv2.imshow("res",res)
 for i,j in enumerate(im):
   cv2.imshow(str(i),j)
+  if i==6:
+  	print(j)
   cv2.imwrite("./1%s.jpg"%i,j)
 cv2.waitKey()
